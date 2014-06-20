70325bc98123cd1786b037299981fb4aadbd0d20
Jeffrey Long <jlong@groupon.com>
2014-06-20 15:50:16 -0500

    XMLHttpRequest helpers

--- public/app.js
+++ public/app.js
@@ -1,1 +1,19 @@
 window.Quartermaster = {};
+
+var getRequest = function(method, url, onload){
+  var request = new XMLHttpRequest();
+  request.onload = onload;
+  request.open(method, url, true);
+  return request;
+}
+
+var ajaxGet = function(url, cb){
+  var request = getRequest('get', url, cb);
+  request.send();
+}
+var ajaxPost = function(url, data, cb){
+  var request = getRequest('post', url, cb);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify(data));
+}
+
