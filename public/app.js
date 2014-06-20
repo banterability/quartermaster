df939f1863c5da85fc18e4f3deaccf8aa1601548
Jeffrey Long <jlong@groupon.com>
2014-06-20 15:56:17 -0500

    Refactor ajax helper

--- public/app.js
+++ public/app.js
@@ -1,27 +1,28 @@
 window.Quartermaster = {};
 
-var getRequest = function(method, url, onload){
-  var request = new XMLHttpRequest();
-  request.onload = onload;
-  request.open(method, url, true);
-  return request;
-}
-
-var ajaxGet = function(url, cb){
-  var request = getRequest('get', url, cb);
-  request.send();
-}
-var ajaxPost = function(url, data, cb){
-  var request = getRequest('post', url, cb);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify(data));
-}
+var Ajax = {
+  get: function(url, cb) {
+    var request = this.createRequest('get', url, cb);
+    return request.send();
+  },
+  post: function(url, data, cb) {
+    var request = this.createRequest('post', url, cb);
+    request.setRequestHeader('Content-Type', 'application/json');
+    return request.send(JSON.stringify(data));
+  },
+  createRequest: function(method, url, cb) {
+    var request = new XMLHttpRequest();
+    request.onload = cb;
+    request.open(method, url, true);
+    return request;
+  }
+};
 
 var addItem = function(ev){
   var inputEl = document.querySelector('input');
   var inputValue = inputEl.value;
   console.log({el: inputEl, value: inputValue});
-  ajaxPost('/items/create', {name: inputValue}, function(){
+  Ajax.post('/items/create', {name: inputValue}, function(){
     inputEl.value = '';
     var newItem = document.createElement('li');
 
@@ -46,7 +47,7 @@ document.querySelector('.submit').addEventListener('click', addItem, false);
 var deleteItem = function(ev){
   var parentEl = ev.currentTarget.parentElement;
   var itemName = parentEl.querySelector('span').textContent;
-  ajaxPost('/items/delete', {name: itemName}, function(){
+  Ajax.post('/items/delete', {name: itemName}, function(){
     parentEl.remove();
   });
 };
