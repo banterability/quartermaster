269890b253776cfd65d3326dc2a8a9412111c8f3
Jeffrey Long <jlong@groupon.com>
2014-06-20 15:51:17 -0500

    Client-side add

--- public/app.js
+++ public/app.js
@@ -17,6 +17,32 @@ var ajaxPost = function(url, data, cb){
   request.send(JSON.stringify(data));
 }
 
+var addItem = function(ev){
+  var inputEl = document.querySelector('input');
+  var inputValue = inputEl.value;
+  console.log({el: inputEl, value: inputValue});
+  ajaxPost('/items/create', {name: inputValue}, function(){
+    inputEl.value = '';
+    var newItem = document.createElement('li');
+
+    var newItemText = document.createElement('span');
+    newItemText.textContent = inputValue;
+
+    var newItemDeleteLink = document.createElement('a');
+    newItemDeleteLink.href = 'javascript:void(0);'
+    newItemDeleteLink.classList.add('delete-item');
+    newItemDeleteLink.textContent = 'x';
+    addDeleteListener(newItemDeleteLink);
+
+    newItem.appendChild(newItemText);
+    newItem.appendChild(newItemDeleteLink);
+
+    document.querySelector('ul').appendChild(newItem);
+  });
+}
+
+document.querySelector('.submit').addEventListener('click', addItem, false);
+
 var deleteItem = function(ev){
   var parentEl = ev.currentTarget.parentElement;
   var itemName = parentEl.querySelector('span').textContent;
