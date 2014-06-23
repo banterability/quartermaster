6f59ea371deee4d6cad8275b163e410520b30c3a
Jeffrey Long <jlong@groupon.com>
2014-06-23 16:55:14 -0500

    Update Hogan; Use smaller template engine on client side

--- public/templates.js
+++ public/templates.js
@@ -1,2 +1,2 @@
-var templates = {};
-templates.list_item = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("<li>");_.b("\n" + i);_.b("  <span>");_.b(_.v(_.f("name",c,p,0)));_.b("</span>");_.b("\n" + i);_.b("  <a class='delete-item' href='javascript:void(0);'>x</a>");_.b("\n" + i);_.b("</li>");return _.fl();;});
+if (!!!templates) var templates = {};
+templates["list_item"] = new Hogan.Template({code: function (c,p,i) { var t=this;t.b(i=i||"");t.b("<li>");t.b("\n" + i);t.b("  <span>");t.b(t.v(t.f("name",c,p,0)));t.b("</span>");t.b("\n" + i);t.b("  <a class='delete-item' href='javascript:void(0);'>x</a>");t.b("\n" + i);t.b("</li>");return t.fl(); },partials: {}, subs: {  }});
